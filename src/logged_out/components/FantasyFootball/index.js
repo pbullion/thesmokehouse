ddbc645eba219e6b6a08d2284810ab85743a6cd2
Patrick Bullion <pbullion@gmail.com>
2025-09-17 17:15:43 -0500

    u

--- src/logged_out/components/FantasyFootball/index.js
+++ src/logged_out/components/FantasyFootball/index.js
@@ -17,15 +17,21 @@ function FantasyFootball({ selectFF }) {
         gap: "0.5rem", // space between items
         padding: "0.5rem",
       }}>
-      <ESPNMatchup leagueName="The League" url={`http://localhost:3001/espn-fantasy-football/${week}/9/1429051163`} />
-      <ESPNMatchup leagueName="Opportune" url={`http://localhost:3001/espn-fantasy-football/${week}/7/204792412`} />
+      <ESPNMatchup
+        leagueName="The League"
+        url={`https://sheline-art-website-api.herokuapp.com/espn-fantasy-football/${week}/9/1429051163`}
+      />
+      <ESPNMatchup
+        leagueName="Opportune"
+        url={`https://sheline-art-website-api.herokuapp.com/espn-fantasy-football/${week}/7/204792412`}
+      />
       <ESPNMatchup
         leagueName="BIGGER dynasty"
-        url={`http://localhost:3001/sleeper-fantasy-football/${week}/UrineSumTrouble/1180568250419814400`}
+        url={`https://sheline-art-website-api.herokuapp.com/sleeper-fantasy-football/${week}/UrineSumTrouble/1180568250419814400`}
       />
       <ESPNMatchup
         leagueName="OG Dirtbag Dynasty"
-        url={`http://localhost:3001/sleeper-fantasy-football/${week}/UrineSumTrouble/1180567903047315456`}
+        url={`https://sheline-art-website-api.herokuapp.com/sleeper-fantasy-football/${week}/UrineSumTrouble/1180567903047315456`}
       />
     </div>
   );
