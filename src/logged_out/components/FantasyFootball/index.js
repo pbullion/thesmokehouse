547f2ced703c1b6cc25a74dca010abf3361c14fc
Patrick Bullion <pbullion@gmail.com>
2025-09-28 12:58:46 -0500

    u

--- src/logged_out/components/FantasyFootball/index.js
+++ src/logged_out/components/FantasyFootball/index.js
@@ -7,7 +7,12 @@ function FantasyFootball({ selectFF }) {
   useEffect(() => {
     selectFF();
   }, [selectFF]);
-  const [week, setWeek] = useState(3);
+  const [week] = useState(4);
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 30000);
+  }, []);
 
   return (
     <div
@@ -23,25 +28,25 @@ function FantasyFootball({ selectFF }) {
       <div style={{ padding: 5, borderRadius: 10, backgroundColor: "#59005dff" }}>
         <ESPNMatchup
           leagueName="The League"
-          url={`https://sheline-art-website-api.herokuapp.com/espn-fantasy-football/${week}/9/1429051163`}
+          url={`https://sheline-art-website-api.herokuapp.com//espn-fantasy-football/${week}/9/1429051163`}
         />
       </div>
       <div style={{ padding: 5, borderRadius: 10, backgroundColor: "#0D47A1" }}>
         <ESPNMatchup
           leagueName="Opportune"
-          url={`https://sheline-art-website-api.herokuapp.com/espn-fantasy-football/${week}/7/204792412`}
+          url={`https://sheline-art-website-api.herokuapp.com//espn-fantasy-football/${week}/7/204792412`}
         />
       </div>
       <div style={{ padding: 5, borderRadius: 10, backgroundColor: "#795548" }}>
         <ESPNMatchup
           leagueName="BIGGER dynasty"
-          url={`https://sheline-art-website-api.herokuapp.com/sleeper-fantasy-football/${week}/UrineSumTrouble/1180568250419814400`}
+          url={`https://sheline-art-website-api.herokuapp.com//sleeper-fantasy-football/${week}/UrineSumTrouble/1180568250419814400`}
         />
       </div>
       <div style={{ padding: 5, borderRadius: 10, backgroundColor: "#263238" }}>
         <ESPNMatchup
           leagueName="OG Dirtbag Dynasty"
-          url={`https://sheline-art-website-api.herokuapp.com/sleeper-fantasy-football/${week}/UrineSumTrouble/1180567903047315456`}
+          url={`https://sheline-art-website-api.herokuapp.com//sleeper-fantasy-football/${week}/UrineSumTrouble/1180567903047315456`}
         />
       </div>
     </div>
